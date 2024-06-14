b91118e8b1c1f41a4f311a1bf8c5f98f1caca15a
Robin Kumar <38507037+robinkumar99@users.noreply.github.com>
2024-06-14 10:50:50 +0530

    first commit

---
+++ env.config.jsx
@@ -0,0 +1,15 @@
+import { DIRECT_PLUGIN, PLUGIN_OPERATIONS } from '@openedx/frontend-plugin-framework';
+const config = {
+  pluginSlots: {
+    footer_slot: {
+      plugins: [
+        {
+          // Hide the default footer
+          op: PLUGIN_OPERATIONS.Hide,
+          widgetId: 'default_contents',
+        },
+      ]
+    }
+  },
+}
+export default config;
